366f31d802652cfc53e19f244d259175479d2f9e
nick <hblxwaz@sohu.com>
2020-03-23 06:57:16 +0800

    '添加归一化'

---
+++ gitfile/归一化.py
@@ -0,0 +1,19 @@
+import numpy as np
+import sklearn.preprocessing as sp
+
+raw_samples = np.array([
+    [17., 100., 4000],
+    [20., 80., 5000],
+    [23., 75., 5500]])
+print(raw_samples)
+
+nor_samples = raw_samples.copy() # 复制一份样本
+
+for row in nor_samples:
+    print(row)
+    abs_sum = abs(row).sum() # 取行元素 绝对值 和
+    print(abs_sum)
+    row /= abs_sum # 行元素除以 行元素约对值和
+
+    print(row)
+print(nor_samples)
